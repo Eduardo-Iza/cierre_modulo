bb40164824c9892be6e0c7f6063bb49f051d5344
Eduardo-Iza <edu70.i@gmail.com>
2021-09-21 17:08:32 -0300

    Todos los ejercicios del cierre del modulo 2

---
+++ autos.js
@@ -0,0 +1,33 @@
+let autos = [
+    {marca: 'Ford',
+    modelo: 'Fiesta',
+    precio: 150000,
+    km: 200,
+    color: 'Azul',
+    cuotas: 12,
+    anio: 2019,
+    patente: 'APL123',
+    vendido: false
+    },
+    {marca: 'Toyota',
+    modelo: 'Corolla',
+    precio: 100000,
+    km: 0,
+    color: 'Blanco',
+    cuotas: 14,
+    anio: 2019,
+    patente: 'JJK116',
+    vendido: false
+    },
+    {marca: 'Peugeot',
+    modelo: '208',
+    precio: 120000,
+    km: 95,
+    color: 'Blanco',
+    cuotas: 18,
+    anio: 2015,
+    patente: 'BGH567',
+    vendido: false
+    }
+];
+module.exports = autos
---
+++ concesionaria.js
@@ -0,0 +1,101 @@
+let autos = require('./autos.js');
+let comprador = {
+    nombre: 'Juan',
+    capacidadDePagoEnCuotas: 8000,
+    capacidadDePagoTotal: 150000
+    };
+
+
+const concesionaria = {
+   autos: autos,
+
+    buscarAuto: function(dato){
+        let resultado = autos.find( array => {
+          //  console.log(dato);
+        return dato == array.patente});
+        //console.log(resultado);
+        if (resultado == undefined){
+            resultado = null
+        };
+        return resultado;
+    },
+
+    venderAuto: function(dato){
+        if(this.buscarAuto(dato) != null ){
+            this.buscarAuto(dato).vendido = true;
+        }
+       console.log('auto '+dato+' vendido');
+    },
+
+    autosParaLaVenta(){
+        let autosAlaVenta = autos.filter(function(autos){
+            return autos.vendido == false
+        });
+        return autosAlaVenta;
+    },
+
+    autosNuevos: function(){
+        let enVenta = this.autosParaLaVenta();
+        let ceroKm = enVenta.filter(function(enVenta){
+            return enVenta.km < 100;
+        });
+        return ceroKm;
+    },
+
+    listaDeVentas: function(){
+        let vendidos = autos.filter(function(autos){
+            return autos.vendido == true
+        });
+        let ventas =[];
+        vendidos.forEach(function(vendidos){
+            ventas.push(vendidos.precio);
+        });
+        return ventas;        
+    },
+
+    totalDeVentas: function(){
+        let arrayVentas = this.listaDeVentas();
+            total = arrayVentas.reduce(function(acum,dato){
+                return acum+dato;
+            },0);
+        return total;
+        
+    },
+
+    puedeComprar: function(auto,persona){
+        if (auto.precio < persona.capacidadDePagoTotal &&
+            (auto.precio/auto.cuotas) < persona.capacidadDePagoEnCuotas){
+            return true;
+        }else{
+           return false;
+        };
+    },
+
+    autosQuePuedeComprar: function(persona){
+        let enVenta = this.autosParaLaVenta();
+        let autosPosibles = enVenta.filter(function(enVenta){
+            return (enVenta.precio < persona.capacidadDePagoTotal &&
+                (enVenta.precio/enVenta.cuotas) < persona.capacidadDePagoEnCuotas);
+        });
+        return autosPosibles;
+    }
+
+};
+
+
+
+//console.log(autos);
+console.log(concesionaria.buscarAuto('BGH567'));
+//concesionaria.venderAuto('BGH567');
+//concesionaria.venderAuto('APL123');
+//console.log(concesionaria.autosParaLaVenta());
+//console.log(concesionaria.autosNuevos());
+//console.log(concesionaria.listaDeVentas());
+//console.log(concesionaria.totalDeVentas());
+//console.log(autos);
+
+//let auto1 = concesionaria.buscarAuto('JJK116');
+//console.log(concesionaria.puedeComprar(auto1,comprador));
+
+//console.log(concesionaria.autosQuePuedeComprar(comprador));
+//console.log(comprador);
